eeb7dd59bd68eff37698b1cdf4ca836dec797527
Facu M <facundoivanmartinez@hotmail.com>
2024-04-22 19:42:30 -0300

    mongodb conected and working

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,16 +11,25 @@ app.use(express.json());
 
 
 
- 
-
-
-app.get('/api', (req, res) => {
-    res.send("<h1> API </h1>");
+app.get('/decks',  (req, res) => {
+    res.send("<h1> Aca estarian mis 'Decks' </h1>");
 })
+
+app.post('/decks', async (req, res) => {
+    const newDeck = new Deck({
+        title: 'esto es un nuevo deck',
+    });
+    await newDeck.save();
+    res.send(newDeck);
+})
+
+
+
+
 
 
 //esperamos a que la promesa de conectarse a la db se haga para ejecutar el backend
 mongoose.connect(`mongodb+srv://facundoivanmartinez:${process.env.DB_PASSWORD}@fullstac-deck-2.98psjhq.mongodb.net/?retryWrites=true&w=majority&appName=fullStac-deck-2`)
-        .then(() => {
-    app.listen(PORT, ()=> { console.log(`listening at ${PORT}`) });
-});
+    .then(() => {
+        app.listen(PORT, () => { console.log(`listening at ${PORT}`) });
+    });
